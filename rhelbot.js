3891a166a950f02ef929a34c883906633bec4f5d
Rhelys <qlverfaillie@gmail.com>
2019-11-01 09:17:56 -0700

    Update rhelbot.js
    Updating channel name syntax

--- rhelbot.js
+++ rhelbot.js
@@ -3,7 +3,6 @@ const tmi = require('tmi.js');
 // Reading secret token from a file
 var fs = require('fs');
 var oauth_token = fs.readFileSync(`./oauth.txt`, "utf8");
-console.log(oauth_token);
 
 // Define configuration options
 const opts = {
@@ -30,9 +29,6 @@ client.on('connected', (address, port) => {
 // Connect to Twitch:
 client.connect();
 
-//function onMessageHandler (target, context, msg, self) {
-//  if (self) { return; } // Ignore messages from the bot
-
 // Running commands from the bot
 client.on('chat', (channel, userstate, message, self) => {
   if (self) return;
@@ -46,13 +42,18 @@ client.on('chat', (channel, userstate, message, self) => {
   }
 
  // Extra Life 2019 command
-  else if (commandName === '!extralife' && channel === 'Rhelys') {
-    client.say('Rhelys', `Extra Life unites thousands of gamers to play games and heal kids by fundraising for local Children's Miracle Network Hospitals across North America. Check out my profile here! https://bit.ly/2N7BPzE`);
+  else if (commandName === '!extralife' && `${channel}` === '#rhelys') {
+    client.say('rhelys', `Extra Life unites thousands of gamers to play games and heal kids by fundraising for local Children's Miracle Network Hospitals across North America. Check out my profile here! https://bit.ly/2N7BPzE`);
     console.log(`* Executed ${commandName} command`);
   }
 });
 
-// Function called when the "dice" command is issued
+/*
+Functions to be utilized in the bot's commands
+Covers all non-debug and non-message functionality for the commands
+*/
+
+// !dice
 function rollDice () {
   const sides = 6;
   return Math.floor(Math.random() * sides) + 1;
