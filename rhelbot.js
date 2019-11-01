6d39ae5be95ee19c7b57e0ab20df597bd216abee
Rhelys <qlverfaillie@gmail.com>
2019-11-01 10:14:13 -0700

    Update rhelbot.js

--- rhelbot.js
+++ rhelbot.js
@@ -22,13 +22,13 @@ const opts = {
 // Create a client with our options
 const client = new tmi.client(opts);
 
+// Connect to Twitch:
+client.connect();
+
 // Validating that we've connected to the Twitch Chat IRC
 client.on('connected', (address, port) => {
   console.log(`* Connected to ${address}:${port}`);
 });
-
-// Connect to Twitch:
-client.connect();
 
 // Running commands from the bot
 client.on('chat', (channel, userstate, message, self) => {
@@ -45,6 +45,11 @@ client.on('chat', (channel, userstate, message, self) => {
  // Extra Life 2019 command
   else if (commandName === '!extralife' && `${channel}` === '#rhelys') {
     client.say('rhelys', `Extra Life unites thousands of gamers to play games and heal kids by fundraising for local Children's Miracle Network Hospitals across North America. Check out my profile here! https://bit.ly/2N7BPzE`);
+    console.log(`* Executed ${commandName} command`);
+  }
+
+  else if (commandName === '!commands') {
+    client.say(`${channel}`, `Supported commands: !dice, !extralife`)
     console.log(`* Executed ${commandName} command`);
   }
 });
